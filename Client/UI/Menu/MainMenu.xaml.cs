29fd259695ac6aab1072a4a82365828fae8d792c
extroed <extroed@gmail.com>
2026-01-03 00:04:41 +0600

    Main Menu added

---
+++ Client/UI/Menu/MainMenu.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Client.UI.Menu;
+
+public partial class MainMenu
+{
+    public MainMenu()
+    {
+        InitializeComponent();
+        Topmost = true;
+    }
+
+    private void LocalGame_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("In develop");
+    }
+
+    private void LanGame_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("In develop");
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
